726d2a44db5c93022952359a842f30111d40f75b
Kwandile Mgenge <kwandilen.mgenge@gmail.com>
2024-11-29 14:44:32 +0200

    HomePage implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './components/pages/HomePage';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<HomePage/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
